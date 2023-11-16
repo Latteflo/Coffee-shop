c5c160cd49938ab5225bd71d2f1d89a8dd4452e0
latteflo <sf.simion.f@gmail.com>
2023-11-16 15:02:19 +0100

    product page

--- src/pages/ProductDetail/ProductDetails.tsx
+++ src/pages/ProductDetail/ProductDetails.tsx
@@ -4,74 +4,92 @@ import { useProducts } from "../../components/ProductsContext/ProductsContext";
 import { useCart } from "../../components/CartContext/CartContext";
 import NotFound from "../NotFound/NotFound";
 import "./productDetail.css";
+import { FaTrash, FaArrowLeft, FaCartPlus, FaCartArrowDown } from 'react-icons/fa';
 
 const ProductDetailPage: React.FC = () => {
-  const { id } = useParams<{ id: string }>();
-  const navigate = useNavigate();
-  const products = useProducts();
-  const { addToCart } = useCart();
+  const { id } = useParams<{ id: string }>()
+  const navigate = useNavigate()
+  const products = useProducts()
+  const { addToCart } = useCart()
 
-  const productId = Number(id);
+  const productId = Number(id)
 
-  const product = products.find((p) => Number(p.id) === productId);
+  const product = products.find((p) => Number(p.id) === productId)
 
-  const [quantity, setQuantity] = useState<number>(1);
-  const [showQuantity, setShowQuantity] = useState<boolean>(false);
+  const [quantity, setQuantity] = useState<number>(1)
+  const [showQuantity, setShowQuantity] = useState<boolean>(false)
 
   const handleAddToCart = () => {
     if (product) {
-      addToCart(quantity); 
-      setShowQuantity(true);
+      addToCart( quantity);   
+      setShowQuantity(true)
+      setQuantity ( quantity+1)
+    }
+  }
+
+  const handleRemoveFromCart = () => {
+    if (product) {
+      addToCart( -quantity);   
+      setShowQuantity(true)
+      setQuantity (0)
+    }
+  }
+  const handleDecreaseQuantity = () => {
+    if (product && quantity > 1) {
+      setQuantity(quantity - 1);
     }
   };
 
   const handleQuantityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setQuantity(Number(e.target.value));
-  };
+    setQuantity(Number(e.target.value))
+  }
 
   const goBackToCategories = () => {
-    navigate("/categories");
-  };
+    navigate("/categories")
+  }
 
   if (!product) {
-    return <NotFound />;
+    return <NotFound />
   }
 
   return (
     <div className="product-detail-container">
+      <button className="go-back-button" onClick={goBackToCategories}>
+        <FaArrowLeft /> Back
+      </button>
       <div className="product-image-section">
-        <img
-          src={product.image}
-          alt={product.title}
-          className="product-image"
-        />
+        <img src={product.image} alt={product.title} className="product-image" />
       </div>
       <div className="product-info-section">
         <h2 className="product-title">{product.title}</h2>
+        <p className="product-price">${product.price}</p>
         <p className="product-description">{product.description}</p>
-        <p className="product-price">Price: ${product.price}</p>
         <div className="quantity-selection">
-          <label htmlFor="quantity">Quantity: </label>
+          <label htmlFor="quantity">Items in the cart: </label>
           <input
             type="number"
             id="quantity"
             value={quantity}
             onChange={handleQuantityChange}
-            min="1"
+            min="0"
           />
         </div>
-        {showQuantity && (
-          <p className="added">Added to cart: {quantity} item(s)</p>
-        )}
-        <button className="add-to-cart-button" onClick={handleAddToCart}>
-          Add to Cart
+        <div className="product-actions">
+          <button className="action-button" onClick={handleAddToCart} aria-label="add to cart">
+            <FaCartPlus />  Add
+          </button>
+          <button className="action-button" onClick={handleDecreaseQuantity} aria-label="remove from cart">
+          <FaCartArrowDown /> Remove
         </button>
-        <button className="go-back-button" onClick={goBackToCategories}>
-          Go Back to Categories
-        </button>
+          <button className="action-button" onClick={handleRemoveFromCart} aria-label="empthy cart">
+            <FaTrash /> Empty
+          </button>
+        </div>
+        {showQuantity}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductDetailPage
+export default ProductDetailPage;
+
--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,22 +1,46 @@
 import React from "react"
 import { useUser } from "../../components/UserContext/UserContext"
+import { useNavigate } from "react-router-dom"
+import { signOut } from 'firebase/auth';
+import { Auth } from "../../api/firebase"
 import "../../index.css"
 import "./profile.css"
 
 const ProfilePage: React.FC = () => {
   const { user } = useUser()
+  const navigate = useNavigate()
 
   if (!user) {
     return <div>Loading...</div>
   }
 
+  const goToShop = () => {
+    navigate('/categories'); 
+  };
+
+  const logOut = async () => {
+    try {
+
+      await signOut(Auth);
+      navigate('/');
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
+
+
   return (
     <div className="profile-container">
       <h1>
-        Hello {user.displayName || "Beloved Coffee Lover"}! Welcome to your
-        profile page!
+        Hello {user.displayName || "Beloved Coffee Lover"}! 
+        <br/>
+        Welcome to your profile page!
       </h1>
       <p> Your email is: {user.email}</p>
+      <div className="buttons">
+        <button onClick={goToShop}>Go to shop</button> 
+        <button onClick={logOut}>Log Out</button>
+      </div>
     </div>
   )
 }
